832f376d97a078aadbd1ad850bc060c0db4495fa
Varun Tanna <varun_tanna@ymail.com>
2022-07-28 20:05:03 -0400

    added query for GETME'

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
-import Switch from "react-bootstrap/esm/Switch";
 
 const client = new ApolloClient({
   request: (operation) => {
