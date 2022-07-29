cc8c2d0f8fd4a2d2eea70623a9fd19215a73b748
Varun Tanna <varun_tanna@ymail.com>
2022-07-28 21:10:19 -0400

    authjs done

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -30,7 +30,7 @@ const LoginForm = () => {
 
     try {
         const {data} = await login({
-          variables{...userFormData}
+          variables: {...userFormData}
         });
         Auth.login(data.login.token);
       }  catch (err) {
